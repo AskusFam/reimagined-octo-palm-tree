d4ebb9b4fc07f251ddc3ec9737ecb42cdcecf114
dagem kebede <dagemk01@gmail.com>
2023-04-08 18:17:37 -0400

    Added getting specific photo and allowing deletion

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PhotosAPIView
+from .views import PhotosAPIView, PhotoRetrieveUpdateDestroy
 #chezdior: best food in dc
 
 urlpatterns = [
     path('', PhotosAPIView.as_view(), name='photos_home'),
+    path('<int:pk>', PhotoRetrieveUpdateDestroy.as_view(), name='photo_id_view')
 ]
