8dcf02de93d94470968b7ea5b171eabed7c0f19b
dagem kebede <dagemk01@gmail.com>
2023-04-11 13:32:03 -0400

    removed pyclient

--- py_client/basic.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-#endpoints = 'http://httpbin.org/cookies/set'
-endpoints = 'http://localhost:8000/api'
-params = {
-    'freeform':'134'
-}
-respomse = requests.get(endpoints)
-print (respomse.json()['message'])
