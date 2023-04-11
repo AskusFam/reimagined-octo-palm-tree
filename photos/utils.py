893c546be4266d6a1c18a0b8fc9bae450a9cab22
dagem kebede <dagemk01@gmail.com>
2023-04-11 17:31:26 -0400

    commenting utils

--- photos/utils.py
+++ photos/utils.py
@@ -8,9 +8,19 @@ env.read_env()
 
 
 def create_photo(serializer):
+    '''
+    This function will create/upload a photo to the azure blob storage
+    It creates a uuid for the for the name of the image which avoids issues related
+    to having the same name. 
+    Args:
+        serializer (serializer): This is a serializer object and holds 
+        all of the data recieved from our API.
+
+    Returns:
+        Serializer: Returns the serialzier objec with the validated data altered.
+    '''
     valid_data = serializer.validated_data  # get unsaved instance of the model
     image = valid_data['image']
-    del serializer.validated_data['image']
     image_name = str(uuid.uuid4())
 
     blob_service_client = BlobServiceClient.from_connection_string(
@@ -25,6 +35,17 @@ def create_photo(serializer):
 
 
 def delete_photo(path):
+    '''
+    This function will delete/destory a photo to the azure blob storage
+    It takes the path of from the object and pulls the name from it. That name 
+    is used to delete the file in the blob.
+    Args:
+        Path (string): This is a serializer object and holds 
+        all of the data recieved from our API.
+
+    Returns:
+        None: Nothing returned from this function.
+    '''
     directory = os.path.dirname(path)
     filename = os.path.basename(path)
 
